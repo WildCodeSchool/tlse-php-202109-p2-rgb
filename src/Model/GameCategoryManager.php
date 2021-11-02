8ae3877c59d9cfc5de7d9b200dd5a4f369da2cbd
Chambre ryan <chambre.ryan@orange.fr>
2021-11-02 15:45:26 +0100

    nothing

--- src/Model/GameCategoryManager.php
+++ src/Model/GameCategoryManager.php
@@ -8,9 +8,9 @@ class GameCategoryManager extends AbstractManager
 {
     public const TABLE = 'game_genre';
 
-    public function selectOneByGenre_id(int $id)
+    public function selectOneByGenreId(int $id)
     {
-        $statement = $this->pdo->query("SELECT * from " . static::TABLE .  " WHERE genre_id = $id");         
+        $statement = $this->pdo->query("SELECT * from " . static::TABLE .  " WHERE genre_id = $id");
         $genres = $statement->fetchAll(PDO::FETCH_ASSOC);
 
         return $genres;
