d51e8f2bed344a552183fe98052326ed332f9bbc
Chambre ryan <chambre.ryan@orange.fr>
2021-11-02 15:47:00 +0100

    nothing

--- src/routes.php
+++ src/routes.php
@@ -8,7 +8,6 @@
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
     '' => ['HomeController', 'index',],
-
     'category' => ['CategoryController', 'index', ['id']],
     'descriptionJeu' => ['DescriptionJeuController', 'index'],
     'items' => ['ItemController', 'index',],
