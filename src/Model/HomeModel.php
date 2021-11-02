c2a3e791f75276bc26199972493dd8dac18daae5
Chambre ryan <chambre.ryan@orange.fr>
2021-11-02 08:45:04 +0100

    fix errors and add description jeu

--- src/Model/HomeModel.php
+++ src/Model/HomeModel.php
@@ -2,7 +2,7 @@
 
 namespace App\Model;
 
-class GenreModel
+class HomeModel
 {
     public function getAll(): array
     {
--- src/routes.php
+++ src/routes.php
@@ -8,6 +8,8 @@
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
     '' => ['HomeController', 'index',],
+    'gameCategory' => ['GameCategoryCrontroller','index'],
+    'descriptionJeu' => ['DescriptionJeuController','index'],
     'items' => ['ItemController', 'index',],
     'items/edit' => ['ItemController', 'edit', ['id']],
     'items/show' => ['ItemController', 'show', ['id']],
