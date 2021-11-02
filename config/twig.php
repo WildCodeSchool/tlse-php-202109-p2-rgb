475ddbb98921b3ea7e9ed9e029cdeccb16bbb953
PerrinGaetan <90518071+PerrinGaetan@users.noreply.github.com>
2021-11-02 11:00:57 +0100

    Delete twig.php
    remove this file because it is useless.
    We'll use controller extends AbstractController

--- config/twig.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
-$twig = new Twig\Environment($loader, ['debug' => true]);
-$twig->addExtension(new Twig\Extension\DebugExtension());
