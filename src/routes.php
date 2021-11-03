1b04d0b9667ea96bdd0d0780a029422f7bfd8d16
PerrinGaetan <perringaetandu31@gmail.com>
2021-11-03 15:44:34 +0100

    Fix on navbar : add route category and descriptionGame who have disappeared don't know why

--- src/routes.php
+++ src/routes.php
@@ -8,6 +8,8 @@
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
     '' => ['HomeController', 'index',],
+    'category' => ['CategoryController', 'index', ['id']],
+    'descriptionGame' => ['DescriptionGameController', 'index'],
     'items' => ['ItemController', 'index',],
     'items/edit' => ['ItemController', 'edit', ['id']],
     'items/show' => ['ItemController', 'show', ['id']],
