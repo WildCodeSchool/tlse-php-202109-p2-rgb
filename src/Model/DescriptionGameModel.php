92590ab2922775aee8865e4d493334bd674b4974
Chambre ryan <chambre.ryan@orange.fr>
2021-11-04 13:09:26 +0100

    add link to database like and game for the descriptionGame page

--- src/Model/DescriptionGameModel.php
+++ src/Model/DescriptionGameModel.php
@@ -9,7 +9,9 @@ class DescriptionGameModel extends AbstractManager
     public function selectLikeById(int $id)
     {
         // prepared request
-        $statement = $this->pdo->prepare("SELECT `like`, game_id FROM " . static::TABLE . " RIGHT JOIN `like` ON id=game_id where `like`='like'");
+        $statement = $this->pdo->prepare(
+            "SELECT `like`, game_id FROM " . static::TABLE . " RIGHT JOIN `like` ON id=game_id where `like`='like'"
+        );
         $statement->bindValue('id', $id, \PDO::PARAM_INT);
         $statement->execute();
 
