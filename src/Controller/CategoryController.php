c15b0f5389fcdc60d34ed046231974235390ec07
Benjamin Deneux <benjamindeneux@hotmail.com>
2021-11-09 14:57:02 +0100

    stylisation de la page category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -11,11 +11,6 @@ class CategoryController extends AbstractController
         $gameManager = new CategoryManager();
         $category = $gameManager->selectByGenre($id);
         $gameInfos = $gameManager->selectAllGamesFromCategoryId($id);
-        // $categoriesId = $gameManager->selectgameByID($id);
-        // foreach ($categoriesId as $categorieId) {
-        //     $namesGameById[] = $gameManager->selectCategoriesByNameIdTag($categorieId['game_id']);
-        //     foreach ($namesGameById as $gameById);
-        // }
         return $this->twig->render('Category/index.html.twig', ['category' => $category, 'gameInfos' => $gameInfos]);
     }
 }
